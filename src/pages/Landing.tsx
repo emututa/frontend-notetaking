7d0ff3b5ec7ab3938a9e6baccdb8eb5f09435e47
emututa <mututaemmanuel52@gmail.com>
2025-11-21 09:15:57 +0200

    solved error on images

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,9 +5,9 @@ const Landing = () => {
   const navigate = useNavigate();
   return (
     <div className="items-center justify-center flex flex-col">
-         <img src="public/imgs/logo.png" alt=""  width={90} height={30} />
+         <img src="/imgs/logo.png" alt=""  width={90} height={30} />
          <h1 className="text-[50px] font-extrabold">TAKE NOTE</h1>
-         <img src='public/imgs/notepad.png' alt="" height={600}  width={350}/>
+         <img src='/imgs/Notepad.png' alt="" height={600}  width={350}/>
          <Button variant='default' onClick={() => navigate('/dashboard')} className="px-[25px] py-[20px] text-[20px] bg-black hover:bg-[#C77D00]  mt-[10px] font-bold">Get Started</Button>
 
     </div>

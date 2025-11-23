3b78707b0abab10c99b47af925507b054b0f2fb6
emututa <mututaemmanuel52@gmail.com>
2025-11-23 15:56:02 +0200

    restoring code were it was

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    // Add proxy to avoid CORS issues
+    // Add proxy to avoid CORS issue
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
